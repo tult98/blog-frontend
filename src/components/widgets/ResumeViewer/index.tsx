de9427006d816bd8e4fcfa9cb3a2cef9dae286c1
Tu Le Thanh <lethanhtu1551998@gmail.com>
2022-02-19 01:15:00 +0700

    feat: add loading state for loading resume

--- src/components/widgets/ResumeViewer/index.tsx
+++ src/components/widgets/ResumeViewer/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import { Document, Page, pdfjs } from 'react-pdf'
 import Icon from '~/components/elements/Icon'
+import LoadingIndicator from '~/components/elements/LoadingIndicator'
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
 
@@ -13,15 +14,24 @@ const ResumeViewer = ({ onClose }: { onClose: () => void }) => {
   const [file, _] = useState('./TuLeThanh_resume.pdf')
 
   const onDocumentLoadSuccess = () => {
-    console.info('Load pdf file success')
+    console.info('pdf file is loaded')
   }
 
   const onDocumentLoadError = (error: any) => {
     console.error(error)
   }
 
+  const renderLoadingComponent = () => {
+    return (
+      <div className="absolute flex flex-col items-center -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
+        <LoadingIndicator />
+        <p>Loading resume...</p>
+      </div>
+    )
+  }
+
   return (
-    <div className="fixed z-30 w-full h-full overflow-y-auto -translate-x-1/2 -translate-y-1/2 bg-white md:z-10 top-1/2 left-1/2 md:w-auto md:h-5/6">
+    <div className="fixed z-30 w-full h-full overflow-y-auto -translate-x-1/2 -translate-y-1/2 bg-white min-w-[612px] md:z-10 top-1/2 left-1/2 md:w-auto md:h-5/6">
       <div className="relative z-20 w-full bg-white">
         <Icon
           name="close"
@@ -32,6 +42,7 @@ const ResumeViewer = ({ onClose }: { onClose: () => void }) => {
       <Document
         file={file}
         options={options}
+        loading={renderLoadingComponent}
         onLoadSuccess={onDocumentLoadSuccess}
         onLoadError={onDocumentLoadError}
       >
