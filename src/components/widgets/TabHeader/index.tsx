fb6d86a4388ad24469efa69a8ecd1d7f43001a80
Tu Le Thanh <lethanhtu1551998@gmail.com>
2022-03-10 22:54:55 +0700

    fix: preload local fonts

--- src/components/widgets/TabHeader/index.tsx
+++ src/components/widgets/TabHeader/index.tsx
@@ -11,12 +11,16 @@ const TabHeader = ({
   const postfix = name ? ` | ${name}` : ''
   return (
     <Head>
+      <title>{`Tu (Bob) Le Thanh${postfix}`}</title>
       <meta name="viewport" content="width=device-width" initial-scale="1" />
       <meta name="author" content="Tu Le Thanh" />
       <meta name="description" content="Blog of Tu Le Thanh" />
       <meta name="keywords" content="Tu Le Thanh, Blog, Portfolio" />
-      <title>{`Tu (Bob) Le Thanh${postfix}`}</title>
       <link rel="icon" href="/favicon.png" />
+      <link rel="preload" as="font" href="/fonts/Calibre-Medium.woff2" />
+      <link rel="preload" as="font" href="/fonts/Calibre-Regular.woff2" />
+      <link rel="preload" as="font" href="/fonts/Calibre-Semibold.woff2" />
+      <link rel="preload" as="font" href="/fonts/SFMono-Regular.woff2" />
       {children}
     </Head>
   )
