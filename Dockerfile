588a360103c1d13c970f8053142398c89cfb6bf3
Tu Le Thanh <lethanhtu1551998@gmail.com>
2022-10-01 18:11:51 +0700

    feat: dockernize the application

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM node:18.10-alpine3.15
+
+WORKDIR /usr/src/app
+
+COPY package.json .
+COPY yarn.lock .
+
+RUN yarn install
+
+COPY . .
+
+EXPOSE 3000
+
+CMD ["yarn",  "dev"]
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,9 @@
 import type { NextPage } from 'next'
-// import { useRouter } from 'next/router'
-// import Link from 'next/link'
 import TabHeader from '~/components/widgets/TabHeader'
-// import { signInWithGoogle } from '~/services/firebase'
 import BlogContainer from '~/components/modules/Blog/BlogContainer'
 import BlogHeader from '~/components/layouts/BlogHeader'
 
 const Home: NextPage = () => {
-  // const router = useRouter()
-
-  // const onSignIn = async () => {
-  //   try {
-  //     await signInWithGoogle()
-  //     router.push('/blogs')
-  //   } catch (error) {
-  //     alert(error)
-  //   }
-  // }
-
   return (
     <>
       <TabHeader name="Blogs" />
