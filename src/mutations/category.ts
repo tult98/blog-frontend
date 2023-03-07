073aa163e6959db3de9749e1d6b800ed688f23d3
Tu Thanh Le <lethanhtu1551998@gmail.com>
2023-03-07 14:33:15 +0700

    fix: cannot delete category issue

--- src/mutations/category.ts
+++ src/mutations/category.ts
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client'
 
 export const DELETE_CATEGORY = gql`
-  mutation DeleteCategory($id: String!) {
+  mutation DeleteCategory($id: ID!) {
     deleteCategory(id: $id) {
       id
     }
