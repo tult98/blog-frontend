a7d16f32ea0a1dacf24d01a53a97ee113c9f93aa
Tu Thanh Le <lethanhtu1551998@gmail.com>
2023-04-17 00:23:02 +0700

    fix: correct styling for header

--- src/components/layouts/BlogHeader/Header.tsx
+++ src/components/layouts/BlogHeader/Header.tsx
@@ -1,21 +1,24 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import Icon from '~/components/elements/Icon'
 
 const Header = () => {
   return (
-    <header className="fixed top-0 z-10 justify-end sm:justify-center navbar bg-base-100">
-      <div className="absolute left-8 hover:cursor-pointer">
-        <Image src="/blog-logo.png" alt="blog-logo" width={120} height={100} />
+    <header className="mb-[33px] sticky top-0 z-10 w-full">
+      <div className="max-w-[1220px] w-full h-[72px] px-10 grid grid-cols-4 items-center">
+        <Link className="col-span-1 hover:cursor-pointer" href="/">
+          <Image src="/blog-logo.png" alt="blog-logo" width={120} height={100} />
+        </Link>
+        <div className="relative col-span-2 py-3 overflow-hidden border border-gray-300 rounded whitespace-nowrap">
+          <Icon name="search" style="w-6 h-6 absolute top-1/2 -translate-y-1/2 left-4 text-[#3E34F3]" />
+          <input
+            type="text"
+            placeholder="What do you want to learn about GraphQL?"
+            className="pl-12 pr-4 rounded input input-bordered focus:outline-none min-w-[15rem] lg:min-w-[30rem]"
+          />
+        </div>
+        <Icon name="menu" style="w-5 h-5 sm:hidden" />
       </div>
-      <div className="relative py-1.5 form-control hidden sm:inline-block">
-        <Icon name="search" style="w-6 h-6 absolute top-1/2 -translate-y-1/2 left-4 text-[#3E34F3]" />
-        <input
-          type="text"
-          placeholder="What do you want to find?"
-          className="pl-12 pr-4 rounded input input-bordered focus:outline-none min-w-[15rem] lg:min-w-[30rem]"
-        />
-      </div>
-      <Icon name="menu" style="w-5 h-5 sm:hidden" />
     </header>
   )
 }
