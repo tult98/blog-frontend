80ef345e67e54db98aba6de26bdbcaeb95b60e64
Tu Thanh Le <lethanhtu1551998@gmail.com>
2023-05-31 00:16:01 +0700

    feat: responsive for search bar

--- src/components/layouts/BlogHeader/SearchBar.tsx
+++ src/components/layouts/BlogHeader/SearchBar.tsx
@@ -11,7 +11,7 @@ const SearchBar = ({ style }: { style?: string }) => {
       classNames={{
         root: `col-span-2 overflow-hidden border border-gray-300 rounded whitespace-nowrap ${style}`,
         form: 'relative w-full h-full py-3',
-        input: 'pl-12 pr-4 rounded input input-bordered focus:outline-none min-w-[15rem] lg:min-w-[30rem] w-full',
+        input: 'pl-12 pr-4 rounded focus:outline-none min-w-[15rem] lg:min-w-[30rem] w-full',
         submitIcon: 'w-6 h-6 absolute top-1/2 -translate-y-1/2 left-2 text-[#3E34F3]',
         loadingIcon: 'w-6 h-6 absolute top-1/2 -translate-y-1/2 left-2 text-[#3E34F3]',
       }}
