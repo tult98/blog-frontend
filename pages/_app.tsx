ea935c9c1347d385243a6195a7075dba26bc3cb1
Tu Thanh Le <tult33@fpt.com>
2023-10-07 10:17:29 +0700

    refactor: remove unused code

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,25 +1,14 @@
-// import algoliasearch from 'algoliasearch/lite'
 import type { AppProps } from 'next/app'
-// import { InstantSearch } from 'react-instantsearch-hooks-web'
 import { RecoilRoot } from 'recoil'
-// import Notification from '~/components/widgets/Notification'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
-  // const searchClient = algoliasearch(
-  //   process.env.NEXT_PUBLIC_SEARCH_APPLICATION_ID ?? '',
-  //   process.env.NEXT_PUBLIC_SEARCH_ONLY_API_KEY ?? '',
-  // )
-
   return (
-    <RecoilRoot>
-      {/* <InstantSearch searchClient={searchClient} indexName={process.env.NEXT_PUBLIC_SEARCH_INDEX_NAME}> */}
-      {/* <Notification /> */}
-      <div className="flex flex-row justify-center">
+    <div className="flex flex-row justify-center">
+      <RecoilRoot>
         <Component {...pageProps} />
-      </div>
-      {/* </InstantSearch> */}
-    </RecoilRoot>
+      </RecoilRoot>
+    </div>
   )
 }
 
--- src/components/widgets/Hit.tsx
+++
@@ -1,9 +0,0 @@
-const Hit = ({ hit }: { hit: any }) => {
-  return (
-    <article className="grid grid-cols-2 hover:bg-[#EFEEF7] hover:cursor-pointer">
-      <p>{hit.name}</p>
-    </article>
-  )
-}
-
-export default Hit
