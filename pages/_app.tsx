63dd8d438bf4a7ca416a251f86a89df534b9ffb7
Tu Thanh Le <tult33@fpt.com>
2023-10-22 00:37:10 +0700

    feat: install google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,34 @@
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
 import '../styles/globals.css'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
-    <div className="flex flex-row justify-center">
-      <RecoilRoot>
-        <Component {...pageProps} />
-      </RecoilRoot>
-    </div>
+    <>
+      <noscript>
+        <iframe
+          src="https://www.googletagmanager.com/ns.html?id=GTM-TNQ5SNSD"
+          height="0"
+          width="0"
+          style={{ display: 'none', visibility: 'hidden' }}
+        ></iframe>
+      </noscript>
+      <body className="flex flex-row justify-center">
+        <RecoilRoot>
+          <Component {...pageProps} />
+        </RecoilRoot>
+      </body>
+      <Script id="google-tag-manager">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TNQ5SNSD');
+        `}
+      </Script>
+    </>
   )
 }
 
