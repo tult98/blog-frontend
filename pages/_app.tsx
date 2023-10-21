fc0a09c15a8a3b23efcda481b1e6eff7e5977ebc
Tu Thanh Le <tult33@fpt.com>
2023-10-22 00:37:10 +0700

    feat: install google tag manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,33 +1,33 @@
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 import { RecoilRoot } from 'recoil'
 import '../styles/globals.css'
-import Script from 'next/script'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
-      <noscript>
-        <iframe
-          src="https://www.googletagmanager.com/ns.html?id=GTM-TNQ5SNSD"
-          height="0"
-          width="0"
-          style={{ display: 'none', visibility: 'hidden' }}
-        ></iframe>
-      </noscript>
+      <Script id="google-tag-manager">
+        {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-TNQ5SNSD');
+        `}
+      </Script>
       <body className="flex flex-row justify-center">
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TNQ5SNSD"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
         <RecoilRoot>
           <Component {...pageProps} />
         </RecoilRoot>
       </body>
-      <Script id="google-tag-manager">
-        {`
-          <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-TNQ5SNSD');</script>
-        `}
-      </Script>
     </>
   )
 }
