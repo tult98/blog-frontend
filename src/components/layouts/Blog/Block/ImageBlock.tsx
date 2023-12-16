7767c3aa0f6b624905ec47e57d9707a958d615fe
Tu Thanh Le <lethanhtu1551998@gmail.com>
2023-12-16 07:48:46 +0700

    fix: handle expire images

--- src/components/layouts/Blog/Block/ImageBlock.tsx
+++ src/components/layouts/Blog/Block/ImageBlock.tsx
@@ -8,13 +8,27 @@ const ImageBlock = ({ block }: { block: ImageBlockObjectResponse }) => {
     [block.image],
   )
 
+  const imageInfo = useMemo(() => {
+    const pattern = /\?w=(\d+)&h=(\d+)/
+
+    const match = imageUrl.match(pattern)
+
+    if (match) {
+      const width = parseInt(match[1])
+      const height = parseInt(match[2])
+
+      return { width, height }
+    }
+    return { width: 0, height: 0 }
+  }, [imageUrl])
+
   return (
     <figure className="flex flex-col py-6">
       <Image
         alt=""
         src={imageUrl}
-        width={0}
-        height={0}
+        width={imageInfo.width}
+        height={imageInfo.height}
         style={{ width: '100%', height: 'auto' }}
         sizes="(min-width: 740px) 644px, 91.9vw"
       />
